53890bcee2bdd817394dde793d73ce1faf418245
Arnav Verma <arnxvv@gmail.com>
2024-12-27 00:42:57 +0530

    input embedding layer

--- model.py
+++ model.py
@@ -1,1 +1,16 @@
-print('hello world')
+import torch 
+from torch import nn
+from math import sqrt
+
+class InputEmbedding(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+
+        self. d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * sqrt(self.d_model)
+    
