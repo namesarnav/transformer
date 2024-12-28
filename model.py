cf48af1ce1109c49ac39ae2275306a9b024d37ce
Arnav <ArnavVerma@my.unt.edu>
2024-12-28 16:38:51 +0530

    added FFN

--- model.py
+++ model.py
@@ -47,4 +47,13 @@ class LayerNormalize(nn.Module):
     def __init__(self, eps: float = 10**-6):
         super().__init__()
         self.eps = eps
-        
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self,x):
+
+        mean = x.mean(dim = -1 , keepDim =True)
+        std = x.std(dim=-1 , keepDim = True)
+
+        return self.alpha * (x-mean) / (std + self.eps) + self.bias
+
