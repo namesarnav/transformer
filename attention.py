f51db31e7b544da3e1aabc1e5b3613d982c0d285
Arnav <ArnavVerma@my.unt.edu>
2024-12-29 22:21:03 +0530

    attention

--- attention.py
+++ attention.py
@@ -5,7 +5,19 @@ from . import *
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, d_model: int, seq_len: int) ->:
+    def __init__(self, d_model: int, h: int, dropout: float) ->None:
         super().__init__()
         self.d_model = d_model 
-        self.seq_
+        self.h = h 
+        
+        assert d_model % h == 0, "d_model is not divisible by h"
+    
+        #d_k = d_model / h
+
+        self.d_k = d_model // h 
+
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        
+        
